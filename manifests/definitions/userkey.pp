1fc8eb17282bcafdae7ed26e3fa330eba203ed60
Craig Carnell <craig_carnell@hotmail.com>
2016-03-16 11:24:48 +0000

    add quick fix for ssh_auth key if absent

--- manifests/definitions/userkey.pp
+++ manifests/definitions/userkey.pp
@@ -1,12 +1,22 @@
 define users::userkey($key, $ensure = present, $type = "ssh-rsa", $comment = "default-key" ) {
                        if ($key != "" ) {
-                         ssh_authorized_key { "${title}_${comment}": 
-                           ensure  => $ensure,
-                           key     => $key,
-                           type    => $type,
-                           user    => $name,
-                           options => $options,
-                           require => [ User[$name], File["/home/${name}/.ssh"], ],
+                         if($ensure == absent) {
+                           ssh_authorized_key { "${title}_${comment}": 
+                             ensure  => $ensure,
+                             key     => $key,
+                             type    => $type,
+                             user    => $name,
+                             options => $options
+                           }
+                         } else {
+                           ssh_authorized_key { "${title}_${comment}": 
+                             ensure  => $ensure,
+                             key     => $key,
+                             type    => $type,
+                             user    => $name,
+                             options => $options,
+                             require => [ User[$name], File["/home/${name}/.ssh"], ],
+                           }
                          }
                        }
 }
